7cdc159d3bdbcca883360502f680c5201245884c
nikolajred <nikolajred@gmail.com>
2019-08-27 21:06:57 +0300

    second solving

--- src/Task_4.java
+++ src/Task_4.java
@@ -1,15 +1,20 @@
+import java.util.ArrayList;
+
 public class Task_4 {
-    public static char char_1;
-    public  static String str_2;
+    public static String str_3 = "";
     public static String str_1 = "Candy";
+
     public static void main(String[] args) {
-
-
+        ArrayList<String> arrayList = new ArrayList<>();
         for (int i = 0; i < str_1.length(); i++) {
-            char_1 = str_1.charAt(i);
-            str_2  = str_2 + char_1 + char_1;
+            String newElement = str_1.substring(i, i + 1);
+            arrayList.add(i, (newElement.concat(newElement)));
+            str_3 += (arrayList.get(i));
+
         }
-        System.out.println(str_2.substring(4));
+
+        System.out.println(arrayList);
+        System.out.println(str_3);
 
     }
 }
